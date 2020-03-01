1e86cb813ae897d86c5ab58d9f39439ea193dda3
Jiazhi Guo <jerrykuo7727@gmail.com>
2020-03-01 14:14:38 +0800

    Refactor the initiating shell script.

--- run.sh
+++ run.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-python3_cmd=python3.6
+python3_cmd=python3
 
-stage=2
-use_gpu=cuda:2
+stage=0
+use_gpu=cuda:0
 
-model=bert  # (bert|xlnet)
-model_path=/home/M10815022/Models/roberta-wwm-ext  #/home/M10815022/Models/roberta-wwm-ext  #/home/M10815022/Models/roberta-base-lm-finetune
+model=bert
+model_path=/home/M10815022/Models/roberta-wwm-ext
 save_path=./models/roberta-1.7.2-woASR
 
-train_datasets="DRCD_train Lee_train Kaggle_train FGC_release_all_train"
-dev_datasets="DRCD_dev Lee_dev Kaggle_dev FGC_release_all_dev"
+train_datasets="DRCD_train Lee_train Kaggle_train FGC_release_all_train DROP_train"
+dev_datasets="DRCD_dev Lee_dev Kaggle_dev FGC_release_all_dev DROP_dev"
 test_datasets="DRCD_test Lee_test Kaggle_test FGC_release_all_test"
-
-#train_datasets="DRCD_train Lee_train Kaggle_train ASR_train FGC_release_all_train DRCD_dev Lee_dev Kaggle_dev ASR_dev DRCD_test Lee_test Kaggle_test ASR_test"
-#dev_datasets="FGC_release_all_dev"
-#test_datasets="FGC_release_all_test"
 
 
 if [ $stage -le 0 ]; then
@@ -48,8 +44,6 @@ if [ $stage -le 1 ]; then
   echo "Preparing train set..."
   $python3_cmd scripts/prepare_${model}_data.py $model_path train $train_datasets || exit 1
 fi
-
-exit 0
 
 
 if [ $stage -le 2 ]; then
